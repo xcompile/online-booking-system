2a4fb646a427c2e037e8649afb8799f3cc6dcb3a
Moritz Neuhaeuser <moritz.neuhaeuser@gmail.com>
2021-09-08 17:24:51 +0800

    feat: initial commit

---
+++ src/test/java/com/hongkongcoder/salon/SalonApplicationTest.java
@@ -0,0 +1,14 @@
+package com.hongkongcoder.salon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonApplicationTest {
+
+	@Test
+	void contextLoads() {
+        
+	}
+
+}
