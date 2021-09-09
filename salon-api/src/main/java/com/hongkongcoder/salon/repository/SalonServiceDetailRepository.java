20fb29877ef4cb313adce95aeb3153559a3b22f8
Moritz Neuhaeuser <moritz.neuhaeuser@gmail.com>
2021-09-09 20:32:55 +0800

    fix: added missing @Repository annotations

--- salon-api/src/main/java/com/hongkongcoder/salon/repository/SalonServiceDetailRepository.java
+++ salon-api/src/main/java/com/hongkongcoder/salon/repository/SalonServiceDetailRepository.java
@@ -1,9 +1,11 @@
 package com.hongkongcoder.salon.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.hongkongcoder.salon.model.SalonServiceDetail;
 
+@Repository
 public interface SalonServiceDetailRepository extends CrudRepository<SalonServiceDetail,Long> {
     
 }
--- salon-api/src/main/java/com/hongkongcoder/salon/repository/SlotRepository.java
+++ salon-api/src/main/java/com/hongkongcoder/salon/repository/SlotRepository.java
@@ -2,9 +2,12 @@ package com.hongkongcoder.salon.repository;
 
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.hongkongcoder.salon.model.Slot;
 
+
+@Repository
 public interface SlotRepository extends CrudRepository<Slot,Long> {
     
 }
